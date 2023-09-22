9f2f8c215ed48920f5c7145ad1e8742837acf97e
ClickyCode <80412763+ClickyCodeDev@users.noreply.github.com>
2023-09-22 12:47:09 -0500

    Update PictureApp.py
    All images now are contained in a list so you can add and delete pictures without changing the code. Made small changes like adding comments to better explain the code.

--- App/PictureApp.py
+++ App/PictureApp.py
@@ -1,36 +1,32 @@
+# Install tkinter and Pillow with pip
 from tkinter import *
-# use pip to install Pillow for this to work
 from PIL import ImageTk, Image 
 
-# you can call this whatever you want
-pic = 1
+# Initialize images and current image
+images = [PhotoImage(file="Subscribe.png"),PhotoImage(file="Like.png")]
+currentImage = 0
 
 win = Tk()
 
-#make sure to use the geometry of your photos +100 on both axis
+# Make sure to use the aspect ratio of your images in pixels and add 100 pixels on both axis for margin
 win.geometry("700x500")
 
-def Change():
-    global pic
-    if pic == 1:
-        canvas.itemconfig(image_container,image=img)
-        pic = 2
+# Updating Images
+def ChangeImage():
+    global currentImage
+    if currentImage == len(images)-1:
+        currentImage = 0
     else:
-        canvas.itemconfig(image_container,image=img2)
-        pic = 1
-
-
+        currentImage += 1
+    canvas.itemconfig(image_container,image=images[currentImage])
+    
+# Initialize Canvas to render images
 canvas = Canvas(win, width=600, height=400)
 canvas.pack()
 canvas.place(anchor='center', relx=0.5, rely=0.5)
 
-#if you have more than 2 files you might want to make it a list
-img = PhotoImage(file="Subscribe.png")
-img2 = PhotoImage(file="Like.png")
-
-    
-change = Button(win,text="Next",command=Change)
+change = Button(win,text="Next",command=ChangeImage)
 change.pack()
-image_container =canvas.create_image(0,0, anchor="nw",image=img)
+image_container =canvas.create_image(0,0, anchor="nw",image=images[currentImage])
 
 win.mainloop()
